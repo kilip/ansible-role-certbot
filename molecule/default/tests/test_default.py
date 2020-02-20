2520ab53547a269fbd194497eb786465927a0b2a
Anthonius Munthi <me@itstoni.com>
2020-02-20 18:37:37 +0800

    fixed error when creating credentials for cloudflare

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -14,6 +14,12 @@ def test_hosts_file(host):
     assert f.user == 'root'
     assert f.group == 'root'
 
+def test_cloudflare_credentials(host):
+    f = host.file('/etc/certbot-dns-cloudflare-credentials.ini')
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
 
 def test_certbot_install(host):
     pkg = host.package('certbot')
